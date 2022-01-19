35669e3e8a2dd937619776ca069d324655500d64
glenchiridza <glenchiridz@gmail.com>
2022-01-19 03:05:43 +0200

    controller

---
+++ src/main/java/com/glencconnect/crm_app/controller/NotificationController.java
@@ -0,0 +1,16 @@
+package com.glencconnect.crm_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class NotificationController {
+
+    @GetMapping("notification")
+    public String notification (Map<String,Object> model) {
+        model.put("message","Well you gotta love java consitently");
+        return "notification";
+    }
+}

e7dee8edcb6c70a89b52d2b89cc582fa5e1b3dd7
glenchiridza <glenchiridz@gmail.com>
2022-01-20 14:25:57 +0200

    webmvcconfiguration

---
+++ src/main/java/com/glencconnect/crm_app/CrmConfig.java
@@ -0,0 +1,32 @@
+package com.glencconnect.crm_app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+//this is an example of what is done for us in the background by the annotation @SpringBootApplication inside
+//the CrmAppApplication
+//!!!!
+
+@Configuration
+public class CrmConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
